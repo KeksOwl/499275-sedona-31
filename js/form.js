22554e245838b3d51263773f49f082e0e365feea
KeksOwl <82191938+KeksOwl@users.noreply.github.com>
2021-07-09 00:47:43 +0700

    Внёс правки по замечаниям проверяющего наставника

--- js/form.js
+++ js/form.js
@@ -1,10 +1,22 @@
 const search = document.querySelector(".btn-search");
 const form = document.querySelector(".search-form");
 
+const arrivalDate = form.querySelector("[name=arrival]");
+const departureDate = form.querySelector("[name=departure]");
+const adults = form.querySelector("[name=adults]");
+
 form.classList.add("search-form-hide");
 
 search.addEventListener("click", function (evt) {
   evt.preventDefault();
   form.classList.toggle("search-form-hide");
   form.classList.add("form-show-animation");
+  form.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !adults.value) {
+    evt.preventDefault();
+    form.classList.add("modal-error");
+  }
 });
